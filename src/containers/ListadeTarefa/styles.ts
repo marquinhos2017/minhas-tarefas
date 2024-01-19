975d99381aa7baa7dbe69670a954f504b483cf64
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-01-19 12:23:06 -0900

    Exiba os resultados de filtragem

--- src/containers/ListadeTarefa/styles.ts
+++ src/containers/ListadeTarefa/styles.ts
@@ -5,3 +5,11 @@ export const Container = styled.main`
   height: 100vh; // Define a altura do container que contem a lista de tarefas e com a ajuda do overflow sera possivel que haja um scroll nessa altura
   overflow-y: scroll; // Traansboardar: todo conteudo qeu utrapassar esse 80%vgh ele sera ocultado e ira para o scroll
 `
+
+export const Resultado = styled.p`
+  display: block;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-size: 18px;
+  font-weight: bold;
+`
